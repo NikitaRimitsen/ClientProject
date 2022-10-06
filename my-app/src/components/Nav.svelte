67a4cbf9c0d1d7399a1e1dee9ae08f95f8dddd2b
opilane <opilane@tthk.ee>
2022-10-06 14:33:11 +0300

    On successful a authentication store the token to session and redirect to home

--- my-app/src/components/Nav.svelte
+++ my-app/src/components/Nav.svelte
@@ -51,7 +51,7 @@
 <nav>
 	<ul>
 		<li><a aria-current="{segment === 'register' ? 'page' : undefined}" href="register">register</a></li>
-
+		<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">Log in</a></li>
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 	</ul>
